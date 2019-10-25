a2685737f31b77394305b6de233d0b00524d6489
威小 <weixiao@mima123.local>
2019-10-25 15:07:04 +0800

    添加sdkframework

---
+++ sdkDemo1.framework/Headers/SayHello.h
@@ -0,0 +1,19 @@
+//
+//  SayHello.h
+//  sdkDemo1
+//
+//  Created by 威小 on 2019/10/25.
+//  Copyright © 2019 威小. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface SayHello : NSObject
+
+- (void)sayHello;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ sdkDemo1.framework/Headers/sdkDemo1.h
@@ -0,0 +1,19 @@
+//
+//  sdkDemo1.h
+//  sdkDemo1
+//
+//  Created by 威小 on 2019/10/25.
+//  Copyright © 2019 威小. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for sdkDemo1.
+FOUNDATION_EXPORT double sdkDemo1VersionNumber;
+
+//! Project version string for sdkDemo1.
+FOUNDATION_EXPORT const unsigned char sdkDemo1VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <sdkDemo1/PublicHeader.h>
+#import <sdkDemo1/SayHello.h>
+
